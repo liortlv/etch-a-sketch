97745f37fbcfec35731e40f729e917a24fcea0a9
Lior <liortal.tal@gmail.com>
2022-03-19 14:54:25 +0200

    Change createGrid function to populateBoard. Add changeSize, colorSquare functions. Add HTML full structure

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const container = document.querySelector(".container");
-createGrid(16);
+function populateBoard(gridSize) {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
-function createGrid(gridSize) {
-    container.setAttribute('style', `grid-template-columns: repeat(${gridSize}, 30px); grid-template-rows: repeat(${gridSize}, 30px);`);
-    for (let i = 0; i < Math.pow(gridSize, 2); i++) {
-        let cell = document.createElement("div");
-        cell.classList.add('cell');
-        container.appendChild(cell);
-    }  
+    let amount = gridSize * gridSize;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "rgb(209, 183, 183)";
+        board.appendChild(square);  /// if does not work try: board.insertAdjacentElemnent("beforeend, square");
+    }
 }
 
+populateBoard(16);
 
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        populateBoard(input);
+    } else {
+        alert("Please enter number betwee 2-100!");
+    }
+}
 
-
+function colorSquare() {
+    this.style.backgroundColor = "rgb(100, 148, 202)";
+}
